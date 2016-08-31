348d7a3a0e2fae9be4d746edcda0e1a8e44b52dd
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-08-31 23:17:10 +0200

    Spaces

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
         .Package(url: "https://github.com/vapor/node.git", majorVersion: 0, minor: 5),
 
         // The actual JSON parser
-		.Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0, minor: 20)        
+        .Package(url: "https://github.com/czechboy0/Jay.git", majorVersion: 0, minor: 20)        
     ]
 )
