93056c0e0ddb587b91bd634489cc1589f3e10816
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-09-01 08:12:41 +0100

    Moved to internal-Jay comment removal, should be faster

--- Sources/JSON/JSON+Parse.swift
+++ Sources/JSON/JSON+Parse.swift
@@ -16,96 +16,11 @@ extension JSON {
             throw JSONError.allowFragmentsNotSupported
         }
         
-        var serialized = serialized
-
+        var parsing: Jay.ParsingOptions = []
         if allowComments {
-            serialized = serialized.commentsRemoved()
+            parsing.formUnion(.allowComments)
         }
-
-        let json = try Jay().jsonFromData(serialized)
+        let json = try Jay(parsing: parsing).jsonFromData(serialized)
         self = JSON(json.toNode())
     }
 }
-
-// MARK: Comment removal
-
-extension Sequence where Iterator.Element == Byte {
-    func commentsRemoved() -> Bytes {
-        var commentsRemoved: Bytes = []
-
-        var previousWasForwardSlash = false
-        var previousWasAsterisk = false
-
-        var multilineComment = false
-        var lineComment = false
-
-        var previousWasBackSlash = false
-        var inString = false
-
-        for byte in self {
-            if multilineComment {
-                if previousWasAsterisk {
-                    previousWasAsterisk = false
-                    switch byte {
-                    case Byte.forwardSlash:
-                        multilineComment = false
-                    default:
-                        break
-                    }
-                } else {
-                    switch byte {
-                    case Byte.asterisk:
-                        previousWasAsterisk = true
-                    default:
-                        break
-                    }
-                }
-            } else if lineComment {
-                switch byte {
-                case Byte.newLine:
-                    lineComment = false
-                default:
-                    break
-                }
-            } else if previousWasForwardSlash {
-                previousWasForwardSlash = false
-
-                switch byte {
-                case Byte.asterisk:
-                    multilineComment = true
-                case Byte.forwardSlash:
-                    lineComment = true
-                default:
-                    commentsRemoved.append(byte)
-                }
-            } else if previousWasBackSlash {
-                previousWasBackSlash = false
-                commentsRemoved.append(byte)
-            } else if inString {
-                switch byte {
-                case Byte.quote:
-                    inString = false
-                    commentsRemoved.append(byte)
-                case Byte.backSlash:
-                    previousWasBackSlash = true
-                    commentsRemoved.append(byte)
-                default:
-                    commentsRemoved.append(byte)
-                }
-            } else {
-                switch byte {
-                case Byte.forwardSlash:
-                    previousWasForwardSlash = true
-                case Byte.quote:
-                    inString = true
-                    commentsRemoved.append(byte)
-                default:
-                    commentsRemoved.append(byte)
-                }
-            }
-        }
-
-        return commentsRemoved
-    }
-}
-
