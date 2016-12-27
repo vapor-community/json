25783a3b06315c36c1d04333babd2b5decf00ed1
Kyle Rokita <kyle.rokita@gmail.com>
2016-12-27 18:44:45 -0500

    Update Sequence+Convertible.swift
    public access for makeJSON() for sequence types

--- Sources/JSON/Sequence+Convertible.swift
+++ Sources/JSON/Sequence+Convertible.swift
@@ -1,7 +1,7 @@
 import Node
 
 extension Sequence where Iterator.Element: NodeRepresentable {
-    func makeJSON() throws -> JSON {
+    public func makeJSON() throws -> JSON {
         return try self.makeNode().converted(to: JSON.self)
     }
 }
