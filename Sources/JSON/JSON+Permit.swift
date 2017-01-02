6898c5fa0dcfc4a54f811bd29c7f6af4645d6486
Maxence Henneron <mhenneron@seamgen.com>
2017-01-01 21:27:55 -0800

    Renamed "params" to "keys"

--- Sources/JSON/JSON+Permit.swift
+++ Sources/JSON/JSON+Permit.swift
@@ -1,11 +1,11 @@
 import Node
 
 extension JSON {
-    public func permit(_ params: [String]) -> JSON {
+    public func permit(_ keys: [String]) -> JSON {
         guard var object = node.object as? [String:Node] else { return self }
         
         object.forEach { key,_ in
-            if(!params.contains(key)) {
+            if(!keys.contains(key)) {
                 object[key] = nil
             }
         }
