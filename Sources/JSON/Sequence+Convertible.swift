4882f05031cee8c3bdf8d6dd6cdb5766933c4865
Chris <chriscombs76@gmail.com>
2017-02-06 16:31:03 +0100

    Update Sequence+Convertible.swift

--- Sources/JSON/Sequence+Convertible.swift
+++ Sources/JSON/Sequence+Convertible.swift
@@ -1,7 +1,8 @@
 import Node
 
-extension Sequence where Iterator.Element: NodeRepresentable {
+extension Sequence where Iterator.Element: JSONRepresentable {
     public func makeJSON() throws -> JSON {
-        return try self.makeNode().converted(to: JSON.self)
+        let mapped = try self.map({ try $0.makeJSON() })
+        return try JSON(node: mapped)
     }
 }
