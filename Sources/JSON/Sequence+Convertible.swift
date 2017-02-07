4e395d564f15086c2bde6b4ec5989fbb6d4d219d
Logan Wright <logan.william.wright@gmail.com>
2017-02-07 10:00:22 +0100

    I was wrong.

--- Sources/JSON/Sequence+Convertible.swift
+++ Sources/JSON/Sequence+Convertible.swift
@@ -2,12 +2,6 @@ import Node
 
 extension Sequence where Iterator.Element: JSONRepresentable {
     public func makeJSON() throws -> JSON {
-        return try JSON(node: self.map({ try $0.makeJSON() }))
-    }
-}
-
-extension Sequence where Iterator.Element: NodeRepresentable {
-    public func makeJSON() throws -> JSON {
-        return try self.makeNode().converted(to: JSON.self)
+        return try JSON(node: self.map { try $0.makeJSON() })
     }
 }
