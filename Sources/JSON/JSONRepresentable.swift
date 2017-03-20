e2bc78e8b1b31e49490fcbc31aee4d8540554025
Logan Wright <logan.william.wright@gmail.com>
2017-03-20 13:36:02 +0100

    removing default initializers

--- Sources/JSON/JSONRepresentable.swift
+++ Sources/JSON/JSONRepresentable.swift
@@ -1,33 +1,11 @@
 import Node
 
-enum JSONRepresentableError: Swift.Error {
-    case missingContext
-    case unknownContext(Context)
-}
-
 public protocol JSONRepresentable: NodeRepresentable {
     func makeJSON() throws -> JSON
 }
 
-extension NodeRepresentable where Self: JSONRepresentable {
-    public func makeNode(in context: Context?) throws -> Node {
-        guard let context = context else { throw JSONRepresentableError.missingContext }
-        guard context.isJSON else { throw JSONRepresentableError.unknownContext(context) }
-        let json = try makeJSON()
-        return Node(json)
-    }
-}
-
 public protocol JSONInitializable: NodeInitializable {
     init(json: JSON) throws
-}
-
-extension NodeInitializable where Self: JSONInitializable {
-    public init(node: Node) throws {
-        guard node.context.isJSON else { throw JSONRepresentableError.unknownContext(node.context) }
-        let json = JSON(node)
-        try self.init(json: json)
-    }
 }
 
 public protocol JSONConvertible: JSONRepresentable, JSONInitializable {}
