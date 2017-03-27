b4d002c1e6fb7a5b1532325a07b49efd78e6f7b1
Logan Wright <logan.william.wright@gmail.com>
2017-03-27 14:11:32 +0100

    beta deps

--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
 import PackageDescription
 
+let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
+
 let package = Package(
     name: "JSON",
     dependencies: [
         // Core protocols, extensions, and functionality
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/core.git", beta),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
+        .Package(url: "https://github.com/vapor/node.git", beta),
 
         // The actual JSON parser
         .Package(url: "https://github.com/DanToml/Jay.git", majorVersion: 1)
