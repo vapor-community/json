70ebb1c1c35201b369ce2240969e814eb5ac2766
Logan Wright <logan.william.wright@gmail.com>
2017-05-17 13:11:48 +0100

    updating for 2.0

--- Package.swift
+++ Package.swift
@@ -1,14 +1,12 @@
 import PackageDescription
 
-let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
-
 let package = Package(
     name: "JSON",
     dependencies: [
         // Core protocols, extensions, and functionality
-        .Package(url: "https://github.com/vapor/core.git", beta),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", beta),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
     ]
 )
